abfb04705835bac8ee819f970bec888b0630e54c
seog-yeongeol <cheoni5417@naver.com>
2026-02-21 17:44:02 +0900

    refactor : 필요없는 라이브러리 제거

--- src/goldV/DeciphermentScript.java
+++ src/goldV/DeciphermentScript.java
@@ -3,9 +3,9 @@ package goldV;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 백준 1593
 public class DeciphermentScript {
 
     public static void main(String[] args) throws IOException {
